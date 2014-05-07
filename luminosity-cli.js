da2a3faf6b89ddd9e86984c9bf7419f3a3b5e777
runningtalus <dave@davemay.org>
2014-05-06 22:27:13 -0600

    Completed Part II.  Need to commence Bonus I: Luminosity Library

--- luminosity-cli.js
+++ luminosity-cli.js
@@ -14,12 +14,12 @@ console.log(colorutil['luminosity'](r,g,b));
 //2) luminosity-cli.js declares the variable colorutil
 //3) node requires colorutil.js 
 //4) colorutil.js assigns a value to module.exports
-//5) the module export stores the single object literal {key:value} pair (or multiple key:values)
-//6) the key is luminosity: the value is the function object/func exp
+//5) the module export stores the single object literal {key:value} pair, or multiple key:values.
+//6) the key is luminosity: the value is the function object/function expressions
 //7) we want the object literal to be available to any file that requires colorutil.js
 //8) we access this via colorutil
 //9) we use dot or bracket notation to access the key luminosity
-//10) we pass in the arguments that the  function object luminosity expects
+//10) we pass in the arguments that the function object luminosity expects
 //11) we wrap this in a console log to log this in terminal
 
 
